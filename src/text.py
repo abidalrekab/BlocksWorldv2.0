c5a8ca3b6458b46a558d21faae996a57c0b8aa80
takanokage <danut.petre@gmail.com>
2018-06-25 11:36:13 -0700

    added a method to calculate the vertices of a regular triangle. print a + at each vertex of the triangle.

--- src/text.py
+++ src/text.py
@@ -2,6 +2,7 @@
 # https://pillow.readthedocs.io/en/5.1.x/reference/ImageDraw.html?highlight=draw
 
 from PIL import Image, ImageDraw
+import math
 
 def drawText(imageSize, imageMode, imageBackground, fileName, fileType, taskList):
     image = Image.new(imageMode, imageSize, imageBackground)
@@ -30,6 +31,27 @@ taskList = [
     ['+', (5, 65)],
     ['+', (5, 75)],
 ]
+
+def regularTriangle(x, y, size):
+    output = []
+
+    x0 = x
+    y0 = y - int(size / math.sqrt(3.0))
+    output.append((x0, y0))
+
+    x1 = x - int(size / 2.0)
+    y1 = y + int(size / math.sqrt(3.0) / 2.0)
+    output.append((x1, y1))
+
+    x2 = x + int(size / 2.0)
+    y2 = y + int(size / math.sqrt(3.0) / 2.0)
+    output.append((x2, y2))
+
+    return output
+
+taskList = []
+for point in regularTriangle(320, 240, 50):
+    taskList.append(['+', point])
 
 fileType = 'PNG'
 fileName = 'text.png'
