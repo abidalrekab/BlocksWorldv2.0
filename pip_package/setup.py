0d7bdb23c21839e1ee6e94d989da786a8d3233d2
takanokage <danut.petre@gmail.com>
2018-07-30 14:28:43 -0700

    tweaked the pip manifest. increased version to 0.0.3.

--- pip_package/setup.py
+++ pip_package/setup.py
@@ -40,5 +40,5 @@ setup(
         'Source code': 'https://github.com/takanokage/blocksWorld',
         'Issues': 'https://github.com/takanokage/blocksWorld/issues',
         },
-    version = '0.0.2',
+    version = '0.0.3',
 )
