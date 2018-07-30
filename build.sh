902d114c1b52f8d6703942ed27ff8aca0425ad5c
takanokage <danut.petre@gmail.com>
2018-07-30 15:34:21 -0700

    no need to copy/delete source files anymore.

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 # The MIT License (MIT)
 
-cp -f ../src/*.py blocksWorld/
-
 python  setup.py bdist_wheel
 # python3 setup.py bdist_wheel
--- clean.sh
+++ clean.sh
@@ -7,4 +7,3 @@ rm -rf dist/
 rm -rf *.egg-info/
 rm -rf blocksWorld/*.pyc
 rm -rf blocksWorld/__pycache__
-rm -rf blocksWorld/draw.py blocksWorld/localTypes.py blocksWorld/polygon.py
