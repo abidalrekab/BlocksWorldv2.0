67fbceea084f7773079613c43f19a40c66dc7ee1
sujith42 <sujith2@pdx.edu>
2018-09-14 13:03:28 -0700

    Test for LocalTypes

--- test/testLocalTypes.py
+++ test/testLocalTypes.py
@@ -1,7 +1,11 @@
 import unittest
 import numpy as np
 
+# from blocksWorld import *
+from numpy import random
+
 from blocksWorld.localTypes import *
+
 
 single_point = [
     Point(10, 10)
@@ -29,7 +33,7 @@ def return_angle(pt1, pt2):
 
     ang1 = np.arctan2(*a[::-1])
     ang2 = np.arctan2(*b[::-1])
-    return np.rad2deg((ang1 - ang2) % (2 * np.pi))
+    return np.rad2deg((ang1 - ang2) % (2 * math.pi))
 
 
 class TestLocalTypes(unittest.TestCase):
@@ -46,12 +50,14 @@ class TestLocalTypes(unittest.TestCase):
                 expected.append(Vertex('T', Point(x=points[i].x, y=points[i].y)))
             result = points2vertices('T', points)
             self.assertEqual(expected, result)
+        print('\npoints2vertices with one point and a lot of points: PASS.')
 
     # Unit Tests for testing rotatePoints module
     # Rotation with a point from the set as centre
     def test_rotatePointsequals1(self):
         # If there is a single point in the set
         self.assertEqual(single_point, rotatePoints(single_point, single_point[0], 90.0))
+        print('\nrotatePoints with one point and the same point as centre: PASS.')
 
         if len(points) == 1:
             self.assertEqual(points, rotatePoints(points, points[0], 90.0))
@@ -68,12 +74,15 @@ class TestLocalTypes(unittest.TestCase):
             for i in range(len(points)):
                 if i == 0:
                     i += 1
-                self.assertTrue(return_angle(points[i], rotated_points[i]), 90)
+                self.assertTrue(return_angle(points[i], rotated_points[i]), math.pi/2.0)
+        print('\nrotatePoints with a set of multiple points: PASS.')
 
     # Rotation with a random point which is not in the set as centre
     def test_rotatePointsequals2(self):
         # If there is a single point in the set
         self.assertNotEqual(points, rotatePoints(single_point, Point(160, 120), 180.0))
+        print('\nrotatePoints with one point and some random point as centre: PASS.')
+
         if len(points) == 1:
             self.assertNotEqual(points, rotatePoints(points, Point(160, 120), 180.0))
 
@@ -89,7 +98,8 @@ class TestLocalTypes(unittest.TestCase):
 
             # Checking the angle between the points
             for i in range(len(points)):
-                self.assertTrue(return_angle(points[i], rotated_points[i]), 180)
+                self.assertTrue(return_angle(points[i], rotated_points[i]), math.pi)
+        print('\nrotatePoints with a set of multiple points and some random point as centre: PASS.')
 
 
 if __name__ == '__main__':
