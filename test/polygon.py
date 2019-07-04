e0556ed71884f790f3b700cae2ad9ce873effa8a
Mohamed Abidalrekab <possaifey@gmail.com>
2019-07-03 21:59:48 -0700

    add os.getcwd()

--- test/polygon.py
+++ test/polygon.py
@@ -9,16 +9,16 @@ import os
 import unittest
 import sys
 
-crtScriptDir = os.path.dirname(sys.argv[0])
+crtScriptDir = os.path.dirname(os.getcwd())
 root = os.path.abspath(crtScriptDir)
 
-outputPath = os.path.join(root, "data/output/polygon")
+outputPath = os.path.join(root, "test/data/output/polygon")
 outputPath = os.path.abspath(outputPath)
 
 if not os.path.exists(outputPath):
     os.makedirs(outputPath)
 
-referencePath = os.path.join(root, "data/reference/polygon")
+referencePath = os.path.join(root, "test/data/reference/polygon")
 referencePath = os.path.abspath(referencePath)
 
 try:
