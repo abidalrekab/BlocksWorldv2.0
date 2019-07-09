25d121e4b371fdb2530d6c0bd357c6d8d2ded163
Mohamed Abidalrekab <possaifey@gmail.com>
2019-07-08 23:40:39 -0700

    update draw.py

--- test/draw.py
+++ test/draw.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
 """
-         This module tests for Valid Image with the given boundaries
-         TODO drawPattern
+    This module tests for Valid Image with the given boundaries
+    TODO drawPattern
 """
-
 import os
 import unittest
 import sys
 
-crtScriptDir = os.path.dirname(os.getcwd())
-root = os.path.abspath(crtScriptDir)
+from test import FileName, root
+from test import drawReferencePath, drawOutputPath
+from dataSet import points
+from test import get_image, Validate
 
-outputPath = os.path.join(root, "test/data/output/draw")
-outputPath = os.path.abspath(outputPath)
-
-if not os.path.exists(outputPath):
-    os.makedirs(outputPath)
-
-referencePath = os.path.join(root, "test/data/reference/draw")
-referencePath = os.path.abspath(referencePath)
+if not os.path.exists(drawOutputPath):
+    os.makedirs(drawOutputPath)
 
 try:
     # try using the installed blocksWorld if available
@@ -30,25 +25,6 @@ except ImportError:
     sys.path.append(blocksWorldPath)
     from blocksWorld import *
 
-imageSize = (640, 480)
-x, y = imageSize
-imageMode = 'L'
-imageBackground = 'white'
-
-fileType = 'PNG'
-
-points = [
-            np.array([5,  5]),
-            np.array([5, 15]),
-            np.array([5, 25]),
-            np.array([5, 35]),
-            np.array([5, 45]),
-            np.array([5, 55]),
-            np.array([5, 65]),
-            np.array([5, 75])
-        ]
-
-
 class TestDraw(unittest.TestCase):
     """
     Plotting images for draw , drawWire and drawSolid
@@ -56,49 +32,71 @@ class TestDraw(unittest.TestCase):
 
     # Result image for draw
     def test_draw(self):
-        image = Image.new(imageMode, imageSize, imageBackground)
-        canvas = ImageDraw.Draw(image)
 
-        for i in range(len(points)):
-            draw(canvas, (points[0], points[1]), 'A')
+        """ Create the file name and its saving path, and specify the reference file to compare to."""
+        fileName = FileName(sys._getframe().f_code.co_name)
+        resultFile    = os.path.join(drawOutputPath, fileName)
+        referenceFile = os.path.join(drawReferencePath, fileName)
 
-        fileName = sys._getframe().f_code.co_name + '.png'
+        ''' This function is to create an empty image with a specific dimension
+            with white background, and black/white colored '''
 
-        image.save(outputPath+"/"+fileName, fileType)
+        image, canvas = get_image('L', (15,90),'white')
+
+        for i in range(len(points) - 1):
+            draw(canvas, (points[i + 0], points[i + 1]), 'A')
+
+        """ saving the file and closing it """
+
+        image.save(resultFile)
         image.close()
 
-        resultFile = outputPath + "/" + fileName
-        referenceFile = referencePath + "/" + fileName
+        """ valiate the resulant file against the reference images"""
 
-        # compare results agains reference data
-        with open(resultFile, "rb") as result:
-            with open(referenceFile, "rb") as reference:
-                self.assertTrue(reference.read() == result.read())
+        Validate(referenceFile, resultFile)
 
     # Result image for drawWire
     def test_drawWire(self):
-        image = Image.new(imageMode, imageSize, imageBackground)
-        canvas = ImageDraw.Draw(image)
 
-        drawWire(canvas, points)
+        """ Create the file name and its saving path, and specify the reference file to compare to."""
 
-        fileName = sys._getframe().f_code.co_name + '.png'
+        fileName = FileName(sys._getframe().f_code.co_name)
+        resultFile    = os.path.join(drawOutputPath, fileName)
+        referenceFile = os.path.join(drawReferencePath, fileName)
 
-        image.save(outputPath + "/" + fileName, fileType)
+        ''' This function is to create an empty image with a specific dimension
+            with white background, and black/white colored '''
+
+        image, canvas = get_image('L',(640,480),'white')
+
+        drawWire(canvas, regularPolygon(3, np.array([160, 120]), 50))
+        drawWire(canvas, regularPolygon(4, np.array([480, 120]), 90))
+        drawWire(canvas, regularPolygon(5, np.array([420, 360]), 60))
+        drawWire(canvas, regularPolygon(6, np.array([160, 360]), 80))
+        drawWire(canvas, regularPolygon(7, np.array([320, 160]), 70))
+
+        """ saving the file and closing it """
+
+        image.save(resultFile)
         image.close()
 
-        resultFile = outputPath + "/" + fileName
-        referenceFile = referencePath + "/" + fileName
+        """ valiate the resulant file against the reference images"""
 
-        # compare results agains reference data
-        with open(resultFile, "rb") as result:
-            with open(referenceFile, "rb") as reference:
-                self.assertTrue(reference.read() == result.read())
+        Validate(referenceFile, resultFile)
 
     # Result image for drawSolid
     def test_drawSolid(self):
-        image = Image.new('RGB', imageSize, imageBackground)
-        canvas = ImageDraw.Draw(image)
+
+        """ Create the file name and its saving path, and specify the reference file to compare to."""
+
+        fileName = FileName(sys._getframe().f_code.co_name)
+        resultFile    = os.path.join(drawOutputPath, fileName)
+        referenceFile = os.path.join(drawReferencePath, fileName)
+
+        ''' This function is to create an empty image with a specific dimension
+            with white background, and black/white colored '''
+
+        image, canvas = get_image('RGB',(640,480),'white')
 
         '''
         for different representations of colors see
@@ -110,18 +108,13 @@ class TestDraw(unittest.TestCase):
         drawSolid(canvas, regularPolygon(6, np.array([160, 360]), 80), 'black')
         drawSolid(canvas, regularPolygon(7, np.array([320, 160]), 70), 'brown')
 
-        fileName = sys._getframe().f_code.co_name + '.png'
-
-        image.save(outputPath + "/" + fileName, fileType)
+        """ saving the file and closing it """
+        image.save(resultFile)
         image.close()
 
-        resultFile = outputPath + "/" + fileName
-        referenceFile = referencePath + "/" + fileName
+        """ valiate the resulant file against the reference images"""
 
-        # compare results agains reference data
-        with open(resultFile, "rb") as result:
-            with open(referenceFile, "rb") as reference:
-                self.assertTrue(reference.read() == result.read())
+        Validate(referenceFile, resultFile)
 
 
 if __name__ == '__main__':
