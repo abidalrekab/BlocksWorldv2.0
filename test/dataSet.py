8416077a82b91ac4cd489bfca096c98c9e67be57
Mohamed Abidalrekab <possaifey@gmail.com>
2019-07-08 23:42:45 -0700

    Update dataSet.py

---
+++ test/dataSet.py
@@ -0,0 +1,11 @@
+import numpy as np
+points = [
+            np.array([5,  5]),
+            np.array([5, 15]),
+            np.array([5, 25]),
+            np.array([5, 35]),
+            np.array([5, 45]),
+            np.array([5, 55]),
+            np.array([5, 65]),
+            np.array([5, 75])
+        ]
