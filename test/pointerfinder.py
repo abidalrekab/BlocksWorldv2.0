f3b1c9122170510f754d1dcfec9a744b92523c61
Mohamed Abidalrekab <possaifey@gmail.com>
2019-12-10 18:24:44 -0800

    save

--- test/pointerfinder.py
+++ test/pointerfinder.py
@@ -1,28 +1,26 @@
 from math import *
 from random import *
+import numpy as np
+from BuildDataSet import *
+from math import *
+from random import randint
+from PathsModule import AggregateOutputPath
+
 def length(p1,p2):
     return sqrt(pow((p1[0]-p2[0]),2)+pow((p1[1]-p2[1]),2))
 
-def pointstransformation(Px1,Px2):
-    point = [Px1,Px2]
-    Px1 = point[0]
-    p2 = point[1]
-    midpoint = [(Px1[0]+p2[0])/2,(Px1[1]+p2[1])/2 ]
-    print(midpoint)
-    NrVertices  = randint(3,6)
-    print(NrVertices)
-    alpha = 2 * pi / NrVertices
-    print(degrees(alpha))
-    rd = round(0.5 * length(Px1, p2) / sin(alpha / 2))
-    deltax = abs(midpoint[0]-Px1[0])
-    deltay = sqrt(rd **2 - deltax **2)
+def pointstransformation(Px1,Px2,NrVertices):
+    midpoint = [(Px1[0]+Px2[0])/2,(Px1[1]+Px2[1])/2 ]
+    alpha = 360 / NrVertices
+    print(alpha)
+    a = length(Px1, Px2)
+    R = a / sin(alpha / 2)
+    deltay = 0.5 * sqrt(4 * R ** 2 - a ** 2)
     cx = midpoint[0]
-    cy1 = Px1[1] - deltay
-    cy2 = Px1[1] + deltay
+    cy1 = midpoint[1] - deltay
+    cy2 = midpoint[1] + deltay
     center = [[cx,cy1],[cx,cy2]]
     print(center)
-    print(length(center,Px1))
-    print(length(center, p2))
     return center
 
 def RotateAndTraslate(p1,p2):
@@ -31,36 +29,106 @@ def RotateAndTraslate(p1,p2):
     if p1[0] < p2[0]:
         px0 = p1
         px1 = p2
-    else:
+    elif p2[0] > p1[0]:
         px0 = p2
         px1 = p1
+    elif p1[0] == p2[0]:
+        if p1[1] < p2[1]:
+            px0 = p1
+            px1 = p2
+        else:
+            px0 = p2
+            px1 = p1
+    elif p1[1] == p2[1]:
+        if p1[0] < p2[0]:
+            px0 = p1
+            px1 = p2
+        else:
+            px0 = p2
+            px1 = p1
 
-    if px0[1] > p2[1]:
+    if px0[1] > px1[1] and px0[0] != px1[0]:
+        mode = 1
         theta1 = atan(abs(px0[1]-px1[1])/abs(px0[0]-px1[0]))
-        xhat1 = (px0[0] - Rotpoint[0])* cos(theta1) - (px0[1] - Rotpoint[1]) * sin(theta1) + Rotpoint[0]
-        yhat1 = (px0[0] - Rotpoint[0]) * sin(theta1) + (px0[1] - Rotpoint[1]) * cos(theta1) + Rotpoint[1]
-        theta2 = atan(abs(px0[1] - px1[1]) / abs(px0[0] - px1[0]))
-        xhat2 = (px1[0] - Rotpoint[0]) * cos(theta2) - (px1[1] - Rotpoint[1]) * sin(theta2) + Rotpoint[0]
-        yhat2 = (px1[0] - Rotpoint[0]) * sin(theta2) + (px1[1] - Rotpoint[1]) * cos(theta2) + Rotpoint[1]
-    elif p2[1] > p1[1]:
+        print("theta", theta1 * (180/pi))
+        xhat1, yhat1 = rot(px0,Rotpoint,theta1)
+        xhat2, yhat2 = rot(px1,Rotpoint,theta1)
+    elif px1[1] > px0[1] and px0[0] != px1[0]:
+        mode = 2
         theta1 = - atan(abs(px0[1] - px1[1]) / abs(px0[0] - px1[0]))
-        xhat1 = (px0[0] - Rotpoint[0]) * cos(theta1) - (px0[1] - Rotpoint[1]) * sin(theta1)
-        yhat1 = (px0[0] - Rotpoint[0]) * sin(theta1) + (px0[1] - Rotpoint[1]) * cos(theta1)
-        theta2 = - atan(abs(px0[1] - px1[1]) / abs(px0[0] - px1[0]))
-        xhat2 = (px1[0] - Rotpoint[0]) * cos(theta2) - (px1[1] - Rotpoint[1]) * sin(theta2)
-        yhat2 = (px1[0] - Rotpoint[0]) * sin(theta2) + (px1[1] - Rotpoint[1]) * cos(theta2)
-    else:
-        pass
-    return [xhat1,yhat1],[xhat2,yhat2], theta1, Rotpoint
+        print("theta", theta1 * (180/pi))
+        xhat1, yhat1 = rot(px0, Rotpoint, theta1)
+        xhat2, yhat2 = rot(px1, Rotpoint, theta1)
+    elif px0[1] == px1[1] :
+        mode = 0
+        xhat1 = px0[0]
+        yhat1 = px0[1]
+        xhat2 = px1[0]
+        yhat2 = px1[1]
+        theta1 = 0
+    elif px0[0] == px1[0]:
+        mode = 4
+        theta1 = pi/2
+        print("theta", theta1 * (180 / pi))
+        xhat1, yhat1 = rot(px0, Rotpoint, theta1)
+        xhat2, yhat2 = rot(px1, Rotpoint, theta1)
 
-def RotateAndTranslateCenter(center, theta, Rotpoint):
-    xhat1 = center[0] * cos(theta) - center[0] * sin(theta) + Rotpoint[0]
-    yhat1 = center[1] * sin(theta) + center[1] * cos(theta) + Rotpoint[1]
+    return [xhat1,yhat1],[xhat2,yhat2], theta1, Rotpoint, mode
+
+def RotateAndTranslateCenter(center, theta, Rotpoint, mode):
+    c1 = center[0]
+    c2 = center[1]
+    if mode == 0:
+        return [c1,c2]
+    elif mode == 1:
+        alpha = -theta
+        cx1, cy1 = rot(c1, Rotpoint, alpha)
+        cx2, cy2 = rot(c2, Rotpoint, alpha)
+        return [[cx1, cy1], [cx2, cy2]]
+
+    elif mode == 2:
+        alpha = - theta
+        cx1, cy1 = rot(c1, Rotpoint, alpha)
+        cx2, cy2 = rot(c2, Rotpoint, alpha)
+        return [[round(cx1), round(cy1)], [round(cx2), round(cy2)]]
+    elif mode == 4:
+        alpha = - theta
+        cx1, cy1 = rot(c1, Rotpoint, alpha)
+        cx2, cy2 = rot(c2, Rotpoint, alpha)
+        return [[round(cx1), round(cy1)], [round(cx2), round(cy2)]]
+
+def rot(px0,Rotpoint, theta1):
+    xhat1 = (px0[0] - Rotpoint[0]) * cos(theta1) - (px0[1] - Rotpoint[1]) * sin(theta1) + Rotpoint[0]
+    yhat1 = (px0[0] - Rotpoint[0]) * sin(theta1) + (px0[1] - Rotpoint[1]) * cos(theta1) + Rotpoint[1]
     return [xhat1,yhat1]
 if __name__ == '__main__':
-    p1hat, p2hat , theta, Rotpoint = RotateAndTraslate([120,100],[200,80])
+    NrVertices  = randint(3,6)
+    p1 = [100,230]
+    p2 = [120,230]
+    p1hat, p2hat , theta, Rotpoint, mode = RotateAndTraslate(p1,p2)
     print("xhat1 {} yhat1 {}, xhat2 {} yhat2 {}".format(p1hat[0],p1hat[1],p2hat[0],p2hat[1]))
-    center = pointstransformation(p1hat, p2hat)
+    center = pointstransformation(p1hat, p2hat,NrVertices)
     print("Before rotation and translation", center)
-    center = RotateAndTranslateCenter(center,theta, Rotpoint)
+    print(length(center[0], p1hat))
+    print(length(center[0], p2hat))
+    print(length(center[1], p1hat))
+    print(length(center[1], p2hat))
+    center = RotateAndTranslateCenter(center,theta, Rotpoint, mode)
     print("After rotation and translation", center)
+    print(length(center[0],p1))
+    print(length(center[0], p2))
+    print(length(center[1], p1))
+    print(length(center[1], p2))
+    if not os.path.exists(AggregateOutputPath):
+        os.makedirs(AggregateOutputPath)
+    NumberOfImages = 1
+    color = 'blue'
+    imageName = 'TestImage' + str(uuid.uuid4()) + '.png'
+    resultFile = os.path.join(AggregateOutputPath, imageName)
+    image, canvas = getImage('L', (640, 480), 'white')
+    drawPoints = [p1,p2,center[0],center[1]]
+    print(drawPoints)
+    for i in range(len(drawPoints) - 1):
+        draw(canvas, (drawPoints[i + 0], drawPoints[i + 1]), 'c')
+    image.save(resultFile)
+    image.close()
