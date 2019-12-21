e07e648260a60e395456710fe151cd8153007383
Mohamed Abidalrekab <possaifey@gmail.com>
2019-12-21 00:06:52 -0800

    sparate function into files to make it easier to follow.

--- test/GenerateAggregateObjects.py
+++ test/GenerateAggregateObjects.py
@@ -1,4 +1,4 @@
-from random import *
+import random
 from PathsModule import AggregateOutputPath
 import os
 import uuid
@@ -12,7 +12,7 @@ from SaveData import Save
 if __name__ == "__main__":
     # The main program parameters
     NumberOfImages = 1                              # the number of images you want [0-100000]
-    NrObjects = randint(3,7)                        # the number of objects in each image [1-10]
+    NrObjects = random.randint(3,7)                        # the number of objects in each image [1-10]
     var = 'True'                                    # is there gap between objects or not.
     colors = ['red', 'blue', 'black', 'yellow']     # choose a set of colors that gonna be used
     # create output directory
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         os.makedirs(AggregateOutputPath)
     # Generate N images and save their information into json file that has the same name as image file
     for idx in range(NumberOfImages):
-        tag = 'TestImage' + str(uuid.uuid4())
+        tag = 'Image(' + str(idx) + ')'+ str(uuid.uuid4())
         imageName = tag + '.png'
         jsonfile = tag
         data = {'layer0':[{"AggregateObjectName":tag, "AggCenter" : [], "AggrObjects":[],
@@ -28,10 +28,10 @@ if __name__ == "__main__":
         resultFile = os.path.join(AggregateOutputPath, imageName)
         image, canvas = getImage('RGB', (640, 480), 'white')
         AggpointsShape, centersShape, RadiusShape,rotationShape, NrVerticesShape  = GenerateAdjecentShapesPoints(NrObjects, var)
-        for idx, points in enumerate(AggpointsShape):
+        for idy, points in enumerate(AggpointsShape):
             color = random.choice(colors)
             drawSolid(canvas, points, color)
-            ObjectName = 'object_' + str(idx)
+            ObjectName = 'object_' + str(idy)
             CreateNewObject(data, ObjectName, centersShape[idx], points, RadiusShape[idx], NrVerticesShape[idx], rotationShape[idx])
             # for i in range(len(points) - 1):
             #     draw(canvas, (points[i + 0], points[i + 1]), str(i))
